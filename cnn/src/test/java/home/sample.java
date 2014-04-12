b82164cf3690c03d225e9165d8d1fa3ddcb5ce72
ismail <ismail.osmanjan@gmail.com>
2014-04-12 12:32:13 -0400

    Initial commit

---
+++ cnn/src/test/java/home/sample.java
@@ -0,0 +1,20 @@
+package home;
+
+
+import base.Base;
+import org.junit.Test;
+
+/**
+ * Created by ismailosmanjan on 4/12/14.
+ */
+public class sample extends Base {
+
+    @Test
+
+   public void Sample() {
+    navigateToUrl("http://www.cnn.com");
+    windowMaximize();
+    }
+
+
+}
---
+++ generic/src/main/java/base/Base.java
@@ -0,0 +1,44 @@
+package base;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * Created by ismailosmanjan on 4/12/14.
+ */
+public class Base {
+    public WebDriver driver = null;
+
+    @Before
+    public void setUp() throws Exception {
+        driver = new FirefoxDriver();
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        //driver.close();
+    }
+    //UTILITY Methods
+    public void navigateToUrl(String url){
+        driver.navigate().to(url);
+    }
+    public void windowMaximize(){
+        driver.manage().window().maximize();
+    }
+    public void navigateBack() {
+        driver.navigate().back();
+    }
+
+    public void clickById(String id) {
+        driver.findElement(By.id(id)).click();
+    }
+
+
+
+}
