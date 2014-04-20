1bc40fddb3f242b37fa08a48dec081e0a1a72d54
ismail <ismail.osmanjan@gmail.com>
2014-04-20 15:04:47 -0400

    saucelab issue

--- generic/src/main/java/base/Base.java
+++ generic/src/main/java/base/Base.java
@@ -4,10 +4,18 @@ package base;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Parameters;
 
+import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
@@ -19,15 +27,48 @@ public class Base {
 
     public WebDriver driver = null;
 
-
-   // @Parameters({"url"})
-
-
+    @Parameters({"useSauceLabs","userName", "key", "os", "browser", "browserVersion","url"})
     @BeforeClass
-    public void setUp() throws Exception {
-        driver = new FirefoxDriver();
+    public void setUp(boolean useSauceLabs,String userName,String key,String os, String browser,String browserVersion,
+                      String url) throws IOException {
+        if(useSauceLabs==true){
+            setUpSauce(userName,key,os,browser,browserVersion,url);
+        }else{ getDriver(browser,url);
+
+        }
+    }
+    //driver to run on SauceLabs
+    public void setUpSauce(String userName,String key,String os, String browser,String browserVersion,
+                           String url) throws IOException {
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setBrowserName(browser);
+        capabilities.setCapability("version", browserVersion);
+        capabilities.setCapability("platform", os);
+        this.driver = new RemoteWebDriver(
+                new URL("http://" + userName + ":" + key + "@ondemand.saucelabs.com:80/wd/hub"),
+                capabilities);
+        driver.navigate().to(url);
         driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
-        navigateToUrl("http://www.cnn.com");
+        driver.manage().window().maximize();
+    }
+    //driver to run on local
+    public WebDriver getDriver(String browser,String url) {
+        if(browser.equalsIgnoreCase("firefox")){
+            driver = new FirefoxDriver();
+        } else if(browser.equalsIgnoreCase("safari")){
+            driver = new SafariDriver();
+        } else if(browser.equalsIgnoreCase("chrome")){
+            System.setProperty("webdriver.chrome.driver", System.setProperty("path","path"));
+            driver = new ChromeDriver();
+        } else if(browser.equalsIgnoreCase("internetexplorer")){
+            driver = new InternetExplorerDriver();
+        }
+        driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+        driver.navigate().to(url);
+        driver.manage().window().maximize();
+
+        return driver;
 
     }
 
